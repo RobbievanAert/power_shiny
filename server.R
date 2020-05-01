4ca99dbde5fc75bf5caebb322f69067ad11cc324
Robbie C. M. van Aert <R.C.M.vanAert@tilburguniversity.edu>
2020-05-01 14:00:59 +0200

    create separate ui.R and server.R files

--- server.R
+++ server.R
@@ -5,77 +5,6 @@
 ##### License: MIT License (Expat)
 ###########################################################################
 
-################
-### PACKAGES ###
-################
-
-library(shiny)
-
-################################################################################
-
-ui <- fluidPage(
-  
-  ### App title
-  titlePanel("Onderscheidend vermogen/power berekenen"),
-  
-  ### Main panel for displaying outputs
-  mainPanel(
-    
-    tags$em("Ontwikkeld door: Tilburg University, MTO departement, Robbie C.M. 
-             van Aert"),
-    
-    tags$br(),
-    tags$br(),
-    
-    tags$em("Deze applicatie kan gebruikt worden door studenten om te oefenen 
-    met het berekenen van het onderscheidend vermogen/power. Opgaven en 
-             bijbehorende oplossingen worden automatisch gegenereerd."),
-    
-    tags$br(),
-    tags$br(),
-    
-    helpText("Een onderzoeker is van plan een \\(\\textit{Z}\\)-toets uit 
-      te voeren en wil weten wat het onderscheidend vermogen/power is van deze 
-      hypothesetoets. Onderstaande gegevens zijn bekend:"),
-    
-    uiOutput("ex"), # List of conditions
-    
-    uiOutput("ques"), # Question depending on one or two-tailed test
-    
-    ### Layout of buttons
-    tags$head(
-      tags$style(HTML('#new{background-color:#555555}')),
-      tags$style(HTML('#new{color:white}')),
-      tags$style(HTML('#sol{background-color:#555555}')),
-      tags$style(HTML('#sol{color:white}'))
-    ),
-    
-    tags$br(),
-    
-    actionButton("new", "Volgende vraag"),
-    
-    actionButton("sol", "Oplossing"),
-    
-    tags$br(),
-    tags$br(),
-    
-    uiOutput("sol1"), # Solution step 1
-    
-    plotOutput("plot1", width = "70%", height = "220px"), # Plot step 1
-    
-    uiOutput("sol2"), # Solution step 2
-    
-    plotOutput("plot2", width = "70%", height = "220px"), # Plot step 2
-    
-    uiOutput("sol3"), # Solution step 3
-    
-    plotOutput("plot3", width = "70%", height = "220px"), # Plot step 3
-    
-    uiOutput("sol4") # Solution step 4
-    
-  )
-)
-
 server <- function(input, output) 
 {
   
@@ -435,5 +364,3 @@ server <- function(input, output)
   })
   
 }
-
-shinyApp(ui, server) # Code to run app
